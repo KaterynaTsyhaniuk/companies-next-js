1fdb39159acff75d62509942fa328ba3b55b5c24
KaterynaTsyhaniuk <153962192+KaterynaTsyhaniuk@users.noreply.github.com>
2025-09-18 10:31:02 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off", // бо Next.js автоматично імпортує React
+      "@next/next/no-img-element": "off", // можна відключити за потреби
+    },
+  },
+];
+
+export default eslintConfig;
+
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+
+      <main>
+        <h1>Your new Next.js project</h1>
+      </main>
+
+
+  );
+}
